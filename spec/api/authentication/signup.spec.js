c49b5fd2ddf4cf44eddb1fee6edd2e7d0d723fb4
Sven Schippers <sven.schippers@seaters.com>
2016-11-14 11:50:25 +0100

    Enhanced api signup unit test
    SV-1879

--- spec/api/authentication/signup.spec.js
+++ spec/api/authentication/signup.spec.js
@@ -2,12 +2,16 @@ require('../../helpers');
 
 describe('PUT /api/auth/signup', function() {
 
-    logRequest('auth/signup');
-
+    //logRequest('auth/signup');
     it('should return 200-OK found on signup of new user', function(done) {
+        var newUser = randomUser();
         client.api.authentication
-            .signup(randomUser())
-            .then(done, apiFail() )
+            .signup(newUser)
+            .then((userData) => {
+              expect(userData['firstName']).toMatch(newUser.firstName);
+              expect(userData['lastName']).toMatch(newUser.lastName);
+              expect(userData['email']).toMatch(newUser.email)
+            }, apiFail() )
             .then(done, shouldNotCatch(done));
     });
 
