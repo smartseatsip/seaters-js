37aa9d0ee352a665aadd76ace56f0de54ac14d7e
Sven Schippers <sven.schippers@seaters.com>
2016-12-14 11:45:16 +0100

    Added wl id input to button test page
    SV-1985

--- examples/join-wl-button/app.js
+++ examples/join-wl-button/app.js
@@ -3,10 +3,9 @@ angular.module('app',[])
     var sdk = $rootScope.SeatersSDK = $window.SeatersSDK;
     var chance = $window.chance;
 
-    var wlId = '40591787-936f-4c8a-b6e6-0a97f7583f69';
+    $rootScope.wlId = '40591787-936f-4c8a-b6e6-0a97f7583f69';
     $rootScope.events = ['dark','teal','white','navy'].map((theme) => {
         return {
-            wlId: wlId,
             name: chance.sentence(),
             location: chance.word() + ', New York',
             img: 'http://lorempixel.com/150/75/',
