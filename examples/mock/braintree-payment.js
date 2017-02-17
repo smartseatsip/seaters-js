d64847b1ee26dc17ccd613b16dd7bc2a86a0fe05
Ben Corne <ben.corne@seaters.com>
2017-02-17 14:04:46 +0100

    add assumptions to braintree example

--- examples/mock/braintree-payment.js
+++ examples/mock/braintree-payment.js
@@ -1,5 +1,12 @@
 var shared = require('./_shared');
 
+/***
+ * Assumptions:
+ * - braintree-wlid points to a WL that is configured using braintree
+ * - the user is logged in, booked some tickets and is able to pay for them
+ *   (both preauthorizations and normal payments)
+ */
+
 shared.client.fanService.getPositionBraintreePaymentInfo('braintree-wlid')
 .then(paymentInfo => console.log('Payment Info that can be used for braintree SDK', paymentInfo))
 .then(shared.exitOK, shared.exitFail);
