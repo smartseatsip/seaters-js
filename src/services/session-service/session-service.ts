5bdc2227202f7e5fce860c3faebae5f1aa555a79
sanderdecoster <sander.decoster@seaters.com>
2017-04-28 08:26:55 +0200

    Fixed linter issues

--- src/services/session-service/session-service.ts
+++ src/services/session-service/session-service.ts
@@ -8,171 +8,178 @@ const AUTH_HEADER = 'Authorization';
 const AUTH_BEARER = 'SeatersBearer';
 
 export enum VALIDATION_ERRORS {
-    WRONG_VALIDATION_CODE
+  WRONG_VALIDATION_CODE
 }
 
 export enum SESSION_STRATEGY {
-    EXPIRE
+  EXPIRE
 }
 
 export class SessionService {
 
-    private currentFan: session.Fan;
+  private currentFan: session.Fan;
 
-    private sessionStrategy: SESSION_STRATEGY;
-    private sessionToken : string = "";
+  private sessionStrategy: SESSION_STRATEGY;
+  private sessionToken: string = '';
 
-    constructor (
-        private seatersApi: SeatersApi,
-        sessionStrategy?: SESSION_STRATEGY
-    ) {
-        this.sessionStrategy = sessionStrategy || SESSION_STRATEGY.EXPIRE;
-    }
+  private validationMessageMapper = seatersExceptionV1MessageMapper({
+    'Wrong validation code': VALIDATION_ERRORS.WRONG_VALIDATION_CODE
+  });
 
-    private applyExpireSessionStrategy (session: session.SessionToken): void {
-        //TODO: replace moment with smaller lib or embed needed functionality
-        var expiration = moment.utc(session.expirationDate);
-        var now = moment();
-        console.log(
-            'session expires on %s (in %s minutes)',
-            session.expirationDate,
-            expiration.diff(now, 'minutes')
-        );
-        setTimeout(
-            () => this.doLogout(),
-            expiration.diff(now, 'milliseconds')
-        );
-    }
+  constructor (
+    private seatersApi: SeatersApi,
+    sessionStrategy?: SESSION_STRATEGY
+  ) {
+    this.sessionStrategy = sessionStrategy || SESSION_STRATEGY.EXPIRE;
+  }
 
-    private finishLogin (authSuccess: AuthenticationSuccess): Promise<session.Fan> {
-        this.setSession({
-            expirationDate: authSuccess.token.expirationDate,
-            token: authSuccess.token.value
-        });
-        return this.setCurrentFan();
-    }
+  /**
+   * Configure the given session to be used. This method is intended for transitional
+   * phase where the SDK is not the one doing the login process (Seaters FanWebApp)
+   *
+   * @param session a valid session that is not expired
+   * @param fan a valid fan object
+   */
+  public configureSession (session: session.SessionToken, fan: session.Fan) {
+    this.setSession(session);
+    this.currentFan = fan;
+  }
 
-    private setSession (session: session.SessionToken): void {
-        this.seatersApi.apiContext.setHeader(AUTH_HEADER, AUTH_BEARER + ' ' + session.token);
-        this.sessionToken = session.token;
-        switch (this.sessionStrategy) {
-            default: this.applyExpireSessionStrategy(session);
-        }
-    }
+  public updateCurrentFan (fan: session.Fan): Promise<session.Fan> {
+    this.currentFan = fan;
+    return Promise.resolve<session.Fan>(this.currentFan);
+  }
 
-    private setCurrentFan (): Promise<session.Fan> {
-        return this.seatersApi.fan.fan()
-        .then(fan => this.currentFan = fan);
-    }
-    
-    /**
-     * Configure the given session to be used. This method is intended for transitional
-     * phase where the SDK is not the one doing the login process (Seaters FanWebApp)
-     * 
-     * @param session a valid session that is not expired
-     * @param fan a valid fan object
-     */
-    public configureSession (session: session.SessionToken, fan: session.Fan) {
-        this.setSession(session);
-        this.currentFan = fan;
-    }
+  doEmailPasswordLogin (email: string, password: string, mfaToken?: string): Promise<session.Fan> {
+    return this.seatersApi.authentication.emailPasswordLogin({
+      email: email,
+      password: password,
+      mfaToken: mfaToken
+    }).then((r) => this.finishLogin(r));
+  }
 
-    public updateCurrentFan (fan : session.Fan): Promise<session.Fan> {
-        this.currentFan = fan;
-        return Promise.resolve<session.Fan>(this.currentFan);
-    }
+  doStoredTokenLogin (storedToken: string, mfaToken?: string): Promise<session.Fan> {
+    return this.seatersApi.authentication.storedTokenLogin({
+      token: storedToken,
+      mfaToken: mfaToken
+    }).then((r) => this.finishLogin(r));
+  }
 
-    doEmailPasswordLogin (email: string, password: string, mfaToken?: string): Promise<session.Fan> {
-        return this.seatersApi.authentication.emailPasswordLogin({
-            email: email,
-            password: password,
-            mfaToken: mfaToken
-        }).then((r) => this.finishLogin(r));
-    }
+  doOAuthCodeLogin (oauthProvider: string, code: string): Promise<session.Fan> {
+    return this.seatersApi.authentication.loginWithOAuthCode(oauthProvider, code)
+      .then((r) => this.finishLogin(r));
+  }
 
-    doStoredTokenLogin (storedToken: string, mfaToken?: string): Promise<session.Fan> {
-        return this.seatersApi.authentication.storedTokenLogin({
-            token: storedToken,
-            mfaToken: mfaToken
-        }).then((r) => this.finishLogin(r));
-    }
+  doLogout () {
+    console.log('[SessionService] doLogout'); // DEBUG
+    this.seatersApi.apiContext.unsetHeader(AUTH_HEADER);
+    this.currentFan = undefined;
+    this.sessionToken = undefined;
+  }
 
-    private doRefreshTokenLogin (refreshToken: string, mfaToken?: string): Promise<session.Fan> {
-        return this.seatersApi.authentication.refreshTokenLogin({
-            token: refreshToken,
-            mfaToken: mfaToken
-        }).then((r) => this.finishLogin(r));
-    }
+  // TODO: handle error case
+  doEmailPasswordSignUp (
+    email: string,
+    password: string,
+    firstname: string,
+    lastname: string,
+    language?: string
+  ): Promise<session.Fan> {
+    return this.seatersApi.authentication.signup({
+      email: email,
+      password: password,
+      firstName: firstname,
+      lastName: lastname,
+      language: language || 'en' // TODO: refer to config setting for default language
+    })
+      .then(() => this.doEmailPasswordLogin(email, password));
+  }
 
-    doOAuthCodeLogin (oauthProvider: string, code: string) : Promise<session.Fan> {
-      return this.seatersApi.authentication.loginWithOAuthCode(oauthProvider, code)
-        .then((r) => this.finishLogin(r));
-    }
+  /**
+   * Validate an email by providing a confirmation code
+   *
+   * @param email The email that you want to validate
+   * @param code The code that validates the email
+   * @returns a Promise that resolves with an updated fan or rejects with a VALIDATION_ERRORS
+   * @see VALIDATION_ERRORS
+   */
+  doEmailValidation (email: string, code: string): Promise<session.Fan> {
+    return this.seatersApi.authentication.validate({
+      email: email,
+      code: code
+    })
+      .then(() => this.setCurrentFan())
+      .catch(this.validationMessageMapper);
+  }
 
-    doLogout () {
-        console.log('[SessionService] doLogout');//DEBUG
-        this.seatersApi.apiContext.unsetHeader(AUTH_HEADER);
-        this.currentFan = undefined;
-        this.sessionToken = undefined;
-    }
+  /**
+   * Validate a phone number by providing a confirmation code
+   *
+   * @param phone The phone number that you want to validate
+   * @param code The code that validates the email
+   * @returns a Promise that resolves with an updated fan or rejects with a VALIDATION_ERRORS
+   * @see VALIDATION_ERRORS
+   */
+  doMobilePhoneNumberValidation (phone: session.PhoneNumber, code: string): Promise<session.Fan> {
+    return this.seatersApi.authentication.validate({
+      mobile: phone,
+      code: code
+    } as MobilePhoneValidationData).catch(this.validationMessageMapper);
+  }
 
-    //TODO: handle error case
-    doEmailPasswordSignUp (email:string, password: string, firstname: string, lastname: string, language?: string) : Promise<session.Fan> {
-        return this.seatersApi.authentication.signup({
-            email: email,
-            password: password,
-            firstName: firstname,
-            lastName: lastname,
-            language: language || 'en' //TODO: refer to config setting for default language
-        })
-        .then(() => this.doEmailPasswordLogin(email, password));
-    }
-
-    private validationMessageMapper = seatersExceptionV1MessageMapper({
-        'Wrong validation code': VALIDATION_ERRORS.WRONG_VALIDATION_CODE
+  doEmailReset (email: string): Promise<void> {
+    return this.seatersApi.authentication.resetEmail({
+      email: email,
+      token: this.sessionToken
     });
+  }
 
-    /**
-     * Validate an email by providing a confirmation code
-     * 
-     * @param email The email that you want to validate
-     * @param code The code that validates the email
-     * @returns a Promise that resolves with an updated fan or rejects with a VALIDATION_ERRORS
-     * @see VALIDATION_ERRORS
-     */
-    doEmailValidation (email: string, code: string): Promise<session.Fan> {
-        return this.seatersApi.authentication.validate({
-            email: email,
-            code: code
-        })
-        .then(() => this.setCurrentFan())
-        .catch(this.validationMessageMapper);
-    }
+  whoami () {
+    return this.currentFan;
+  }
 
-    /**
-     * Validate a phone number by providing a confirmation code
-     * 
-     * @param phone The phone number that you want to validate
-     * @param code The code that validates the email
-     * @returns a Promise that resolves with an updated fan or rejects with a VALIDATION_ERRORS
-     * @see VALIDATION_ERRORS
-     */
-    doMobilePhoneNumberValidation (phone: session.PhoneNumber, code: string): Promise<session.Fan> {
-        return this.seatersApi.authentication.validate(<MobilePhoneValidationData> {
-            mobile: phone,
-            code: code
-        }).catch(this.validationMessageMapper);
-    }
+  private applyExpireSessionStrategy (session: session.SessionToken): void {
+    // TODO: replace moment with smaller lib or embed needed functionality
+    let expiration = moment.utc(session.expirationDate);
+    let now = moment();
+    console.log(
+      'session expires on %s (in %s minutes)',
+      session.expirationDate,
+      expiration.diff(now, 'minutes')
+    );
+    setTimeout(
+      () => this.doLogout(),
+      expiration.diff(now, 'milliseconds')
+    );
+  }
 
-    doEmailReset (email: string): Promise<void> {
-      return this.seatersApi.authentication.resetEmail({
-        email: email,
-        token: this.sessionToken
-      });
-    }
+  private finishLogin (authSuccess: AuthenticationSuccess): Promise<session.Fan> {
+    this.setSession({
+      expirationDate: authSuccess.token.expirationDate,
+      token: authSuccess.token.value
+    });
+    return this.setCurrentFan();
+  }
 
-    whoami () {
-        return this.currentFan;
+  private setSession (session: session.SessionToken): void {
+    this.seatersApi.apiContext.setHeader(AUTH_HEADER, AUTH_BEARER + ' ' + session.token);
+    this.sessionToken = session.token;
+    switch (this.sessionStrategy) {
+      default:
+        this.applyExpireSessionStrategy(session);
     }
+  }
+
+  private setCurrentFan (): Promise<session.Fan> {
+    return this.seatersApi.fan.fan()
+      .then(fan => this.currentFan = fan);
+  }
+
+  private doRefreshTokenLogin (refreshToken: string, mfaToken?: string): Promise<session.Fan> {
+    return this.seatersApi.authentication.refreshTokenLogin({
+      token: refreshToken,
+      mfaToken: mfaToken
+    }).then((r) => this.finishLogin(r));
+  }
 
 }
