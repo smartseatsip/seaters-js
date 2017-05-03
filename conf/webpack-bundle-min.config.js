dd67f061db3da685e77fa10fb17bfa4716d4b2e5
sanderdecoster <sander.decoster@seaters.com>
2017-05-03 15:51:27 +0200

    Removed unused webpack config

--- conf/webpack-bundle-min.config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var bundleConfig = require('./webpack-bundle.config');
-bundleConfig.plugins = [];
-bundleConfig.output.filename = bundleConfig.output.filename.replace('.js', '.min.js');
-delete bundleConfig.devtool;
-
-module.exports = bundleConfig;
