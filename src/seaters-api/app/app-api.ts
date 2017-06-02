9c744edd9ea5aa7f2ef6156d51d5fb042926381e
Ben Corne <ben.corne@seaters.com>
2017-06-02 12:17:11 +0200

    added app languages, timezones translations, currencies abstractions

--- src/seaters-api/app/app-api.ts
+++ src/seaters-api/app/app-api.ts
@@ -2,7 +2,8 @@
 import { SeatersApiContext } from '../seaters-api-context';
 import { PagedResult } from '../paged-result';
 import { PagingOptions } from '../paging-options';
-import { Env, Country } from './app-types';
+import { Env, Country, Currency, Translation, TimeZone, Language,
+  TRANSLATION_TARGET } from './app-types';
 
 export class AppApi {
 
@@ -17,6 +18,28 @@ export class AppApi {
   countries (pagingOptions?: PagingOptions): Promise<PagedResult<Country>> {
     let queryParams = SeatersApiContext.buildPagingQueryParams(pagingOptions);
     return this.apiContext.get('/app/countries', null, queryParams);
+  }
+
+  languages (pagingOptions?: PagingOptions): Promise<PagedResult<Language>> {
+    let queryParams = SeatersApiContext.buildPagingQueryParams(pagingOptions);
+    return this.apiContext.get('/app/languages', null, queryParams);
+  }
+
+  timeZones (pagingOptions?: PagingOptions): Promise<PagedResult<TimeZone>> {
+    let queryParams = SeatersApiContext.buildPagingQueryParams(pagingOptions);
+    return this.apiContext.get('/app/time-zones', null, queryParams);
+  }
+
+  translations (target?: TRANSLATION_TARGET, language?: string, pagingOptions?: PagingOptions): Promise<PagedResult<Translation>> {
+    let queryParams = SeatersApiContext.buildPagingQueryParams(pagingOptions);
+    if (target) { queryParams.target = target; }
+    if (language) { queryParams.language = language; }
+    return this.apiContext.get('/app/translations', null, queryParams);
+  }
+
+  currencies (pagingOptions?: PagingOptions): Promise<PagedResult<Currency>> {
+    let queryParams = SeatersApiContext.buildPagingQueryParams(pagingOptions);
+    return this.apiContext.get('/app/currencies', null, queryParams);
   }
 
 }
--- src/seaters-api/app/app-types.ts
+++ src/seaters-api/app/app-types.ts
@@ -1,3 +1,52 @@
+export interface AlgoliaConfiguration {
+  appId: string;
+  apiKey: string;
+  indexName: string;
+  attributes: Object;
+}
+
+export interface BuildInfo {
+  commitId: string;
+  version: string;
+}
+
+export interface Env {
+  /**
+   * public facebook app id, required by facebook SDK
+   */
+  facebookAppId: string;
+
+  /**
+   * public google project number, required by google SDK
+   */
+  googleProjectNumber: string;
+
+  /**
+   * Which environment is this api serving
+   */
+  deployTarget: string;
+
+  /**
+   * Algolia configuration containing public api key, index names and indexed attributes
+   */
+  algoliaConfiguration: AlgoliaConfiguration;
+
+  /**
+   * Public key to access google maps's API
+   */
+  googleMapsApiKey: string;
+
+  /**
+   * Public key to access timezonedb's API
+   */
+  timezonedbApiKey: string;
+
+  /**
+   * API Build Info
+   */
+  buildInfo: BuildInfo;
+}
+
 export interface Country {
   /**
    * Country's ISO 3166 alpha2-code.
@@ -15,24 +64,65 @@ export interface Country {
   name: string;
 }
 
-export interface AlgoliaConfiguration {
-  appId: string;
-  apiKey: string;
-  indexName: string;
-  attributes: Object;
+export interface TimeZone {
+  /**
+   * Time zone id (names found on https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
+   */
+  id: string;
+
 }
 
-export interface BuildInfo {
-  commitId: string;
-  version: string;
+export interface Language {
+  /**
+   * Country of the Language (not a proper locale)
+   * Format: alpha-2 country code
+   */
+  locale: string;
+
+  /**
+   * Translation mapping of country of locale to the name of the language in that locale
+   */
+  name: {[key: string]: string};
 }
 
-export interface Env {
-  facebookAppId: string;
-  googleProjectNumber: string;
-  deployTarget: string;
-  algoliaConfiguration: AlgoliaConfiguration;
-  googleMapsApiKey: string;
-  timezonedbApiKey: string;
-  buildInfo: BuildInfo;
+export interface Currency {
+  /**
+   * Currency code (example USD, EUR)
+   */
+  code: string;
+
+  /**
+   * Currency symbol (example $, €)
+   */
+  symbol: string;
+}
+
+export type TRANSLATION_TARGET = 'WEB' | 'IOS' | 'ANDROID' | 'ALL' | 'COCKPIT' | 'DASHBOARD';
+
+export interface Translation {
+  /**
+   * Translated text
+   */
+  text: string;
+
+  /**
+   * Translation key - unique for the given target
+   */
+  key: string;
+
+  /**
+   * Systemwide unique translation identifier
+   */
+  id: string;
+
+  /**
+   * Context in which the translation is used
+   */
+  target: TRANSLATION_TARGET;
+
+  /**
+   * Language of the translation
+   * Format: alpha-2 country code
+   */
+  lang: string;
 }
