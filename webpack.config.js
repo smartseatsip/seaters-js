c042945a3cac1f80a9c59eface2abf3624189e72
sanderdecoster <sander.decoster@seaters.com>
2017-06-08 09:37:47 +0200

    Updated webpack build Added TSLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,30 @@
+const webpackMerge = require('webpack-merge');
+const args = require('minimist')(process.argv.slice(5));
+
 const sharedModules = [
+
+  // TSLint
+  {
+    test: /\.ts$/,
+    enforce: 'pre',
+    loader: 'tslint-loader',
+    options: {
+      configuration: require('./tslint.json'),
+      fix: true
+    }
+  },
+
+  // ESLint
+  {
+    test: /\.js$/,
+    enforce: 'pre',
+    loader: 'eslint-loader',
+    options: {
+      fix: true,
+      cache: true
+    }
+  },
+
   // Typescript
   {
     test: /\.tsx?$/,
@@ -29,30 +55,32 @@ const sharedModules = [
   }
 ];
 
-module.exports = [
+const sharedConfig = {
+  entry: './index',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: sharedModules.concat([])
+  }
+};
 
+const moduleConfigs = [
   // Module
-  {
+  webpackMerge(sharedConfig, {
 
     context: `${__dirname}/src`,
-    entry: './index',
     target: 'node',
     output: {
       path: `${__dirname}/dist`,
       filename: 'seaters.module.js',
       libraryTarget: 'commonjs'
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx']
-    },
-    devtool: 'inline-source-map',
-    module: {
-      rules: sharedModules.concat([])
     }
-  },
+  }),
 
   // Mock data module
-  {
+  webpackMerge(sharedConfig, {
 
     context: `${__dirname}/mock-data`,
     entry: './index',
@@ -61,61 +89,41 @@ module.exports = [
       path: `${__dirname}/dist`,
       filename: 'seaters-mock-data.module.js',
       libraryTarget: 'commonjs'
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx']
-    },
-    devtool: 'inline-source-map',
-    module: {
-      rules: sharedModules.concat([
-
-      ])
     }
-  },
+  })
+];
 
+const bundleConfigs = [
   // Bundle
-  {
+  webpackMerge(sharedConfig, {
 
     context: `${__dirname}/src`,
     entry: './index',
-    target: 'node',
     output: {
       path: `${__dirname}/dist`,
       filename: 'seaters.bundle.js',
       libraryTarget: 'var',
       library: 'SeatersSDK'
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx']
-    },
-    devtool: 'inline-source-map',
-    module: {
-      rules: sharedModules.concat([
-
-      ])
     }
-  },
+  }),
 
   // Mock data bundle
-  {
+  webpackMerge(sharedConfig, {
 
     context: `${__dirname}/mock-data`,
     entry: './index',
-    target: 'node',
     output: {
       path: `${__dirname}/dist`,
       filename: 'seaters-mock-data.bundle.js',
       libraryTarget: 'var',
       library: 'SeatersSDKMockData'
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx']
-    },
-    devtool: 'inline-source-map',
-    module: {
-      rules: sharedModules.concat([
-
-      ])
     }
-  }
+  })
 ];
+
+let mergedConfigs = moduleConfigs;
+if (args.build) {
+  mergedConfigs.concat(bundleConfigs);
+}
+
+module.exports = mergedConfigs;
