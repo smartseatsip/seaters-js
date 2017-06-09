7e5b1bdb6b89a76a1f5496e7e50a24cd94cd00c8
Sander Decoster <sander.decoster@gmail.com>
2017-06-09 10:57:39 +0200

    Removed debug

--- src/seaters-client.ts
+++ src/seaters-client.ts
@@ -26,7 +26,6 @@ export class SeatersClient {
 
   constructor (options?: SeatersClientOptions) {
     options = Object.assign({}, SeatersClient.DEFAULT_OPTIONS, options);
-    console.log('Creating driver with ', options);
     let requestDriver = getRequestDriver(options.requestDriver);
 
     this.seatersApi = new SeatersApi(options.apiPrefix, requestDriver);
