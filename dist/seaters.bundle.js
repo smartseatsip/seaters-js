b96fe1867a3df7c6983b6993dd5f1fef0e9fe083
Ben Corne <ben.corne@seaters.com>
2017-06-27 14:06:21 +0200

    version 1.20.9

--- dist/seaters.bundle.js
+++ dist/seaters.bundle.js
@@ -1398,7 +1398,7 @@ function __export(m) {
     }
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.version = '1.20.8';
+exports.version = '1.20.9';
 __export(__webpack_require__(17));
 var fan_types_1 = __webpack_require__(2);
 exports.fan = fan_types_1.fan;
