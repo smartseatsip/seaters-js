b7b9cc915c09c5823b7cadf93728fa4e1b1e7a29
sanderdecoster <sander.decoster@seaters.com>
2017-09-07 16:17:55 +0200

    Added more documentation

--- src/seaters-api/profiling/profiling-api.ts
+++ src/seaters-api/profiling/profiling-api.ts
@@ -6,7 +6,47 @@ export class ProfilingApi {
 
   }
 
-  getCategories(): Promise<Category[]> {
+  /**
+   *  CATEGORIES
+   */
+
+  getCategories (): Promise<Category[]> {
     return this.apiContext.get('/v1/categories', {}, {});
   }
+
+  /**
+   *  CATEGORY
+   */
+
+  /**
+   *  INTERESTS
+   */
+
+  /**
+   *  INTEREST
+   */
+
+  /**
+   *  EXTERNAL IDENTIFIERS
+   */
+
+  /**
+   *  EXTERNAL IDENTIFIER
+   */
+
+  /**
+   *  FAN ATTRIBUTES
+   */
+
+  /**
+   *  FAN ATTRIBUTE
+   */
+
+  /**
+   *  USERS
+   */
+
+  /**
+   *  USER
+   */
 }
