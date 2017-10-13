8043d6fea217c42e75df617f66cb1e68022b01df
Sander Decoster <sander.decoster@gmail.com>
2017-10-13 12:17:09 +0200

    Dist

--- dist/seaters.bundle.js
+++ dist/seaters.bundle.js
@@ -61,7 +61,7 @@ var SeatersSDK = /******/ (function(modules) {
     return Object.prototype.hasOwnProperty.call(object, property);
   }; // __webpack_public_path__
   /******/
-  /******/ /******/ __webpack_require__.p = '/Users/sanderdecoster/local_projects/seaters/seaters-js/dist'; // Load entry module and return exports
+  /******/ /******/ __webpack_require__.p = 'C:\\local_projects\\seaters\\seaters-js/dist'; // Load entry module and return exports
   /******/
   /******/ /******/ return __webpack_require__((__webpack_require__.s = 16));
   /******/
@@ -1695,7 +1695,7 @@ var SeatersSDK = /******/ (function(modules) {
       Object.defineProperty(exports, '__esModule', { value: true });
       //noinspection TsLint
       // tslint:disable-next-line
-      exports.version = '1.25.3';
+      exports.version = '1.25.4';
       __export(__webpack_require__(17));
       var fan_types_1 = __webpack_require__(2);
       exports.fan = fan_types_1.fan;
@@ -2557,10 +2557,10 @@ var SeatersSDK = /******/ (function(modules) {
             throw err;
           } else if (err.type !== 'validation_error_v1') {
             console.error('[seatersExceptionV1MessageMapper] invoked with non-v1 exception', err);
-            throw err;
+            return Promise.reject(err);
           } else if (!mapping.hasOwnProperty(err.message)) {
             console.error('[seatersExceptionV1MessageMapper] unmapped v1 error: %s', err.message, err);
-            throw err;
+            return Promise.reject(err);
           } else {
             return Promise.reject(mapping[err.message]);
           }
