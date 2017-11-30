db47edb7b659057e92a6727f9e9ad422748d0107
Azzawi <mahmoud.alazzawi@seaters.com>
2017-11-30 14:01:16 +0100

    Bump version

--- dist/seaters.bundle.js
+++ dist/seaters.bundle.js
@@ -1907,7 +1907,7 @@ var SeatersSDK = /******/ (function(modules) {
       Object.defineProperty(exports, '__esModule', { value: true });
       //noinspection TsLint
       // tslint:disable-next-line
-      exports.version = '1.28.1';
+      exports.version = '1.28.2';
       __export(__webpack_require__(19));
       var fan_types_1 = __webpack_require__(3);
       exports.fan = fan_types_1.fan;
