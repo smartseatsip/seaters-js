26515239b442fd44f7bf79455b2bfd0387eadab1
Sander Decoster <sander.decoster@gmail.com>
2018-01-17 15:40:54 +0100

    Added /fan endpoint

--- dist/seaters.bundle.js
+++ dist/seaters.bundle.js
@@ -61,7 +61,7 @@ var SeatersSDK = /******/ (function(modules) {
     return Object.prototype.hasOwnProperty.call(object, property);
   }; // __webpack_public_path__
   /******/
-  /******/ /******/ __webpack_require__.p = '/Users/mahmoudalazzawi/projects/seaters-js/dist'; // Load entry module and return exports
+  /******/ /******/ __webpack_require__.p = 'C:\\local_projects\\seaters\\seaters-js/dist'; // Load entry module and return exports
   /******/
   /******/ /******/ return __webpack_require__((__webpack_require__.s = 21));
   /******/
@@ -2093,7 +2093,7 @@ var SeatersSDK = /******/ (function(modules) {
       Object.defineProperty(exports, '__esModule', { value: true });
       //noinspection TsLint
       // tslint:disable-next-line
-      exports.version = '1.30.2';
+      exports.version = '1.30.3';
       __export(__webpack_require__(22));
       var fan_types_1 = __webpack_require__(2);
       exports.fan = fan_types_1.fan;
@@ -3665,6 +3665,13 @@ var SeatersSDK = /******/ (function(modules) {
         /**
      * FANS
      */
+        /**
+     * Get current logged in fan
+     * @returns {fan.Fan}
+     */
+        FanService.prototype.fan = function() {
+          return this.seatersApi.fan.fan();
+        };
         /**
      * Send a new SMS containing the code needed to validate email / phone.
      * @param phone
