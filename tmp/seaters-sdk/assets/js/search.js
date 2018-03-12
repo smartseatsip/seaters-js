69ffee40f3da978089906fc5ace48e87697d39ca
Sander Decoster <sander.decoster@gmail.com>
2018-03-12 14:20:52 +0100

    Fixed build script

---
+++ tmp/seaters-sdk/assets/js/search.js
@@ -0,0 +1,200 @@
+var typedoc = typedoc || {};
+typedoc.search = typedoc.search || {};
+typedoc.search.data = {
+  kinds: { '32': 'Variable', '65536': 'Type literal' },
+  rows: [
+    {
+      id: 0,
+      kind: 65536,
+      name: '__type',
+      url: 'globals.html#observer.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported',
+      parent: 'Observer'
+    },
+    {
+      id: 1,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/apirequestdefinition.html#headers.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'ApiRequestDefinition.headers'
+    },
+    {
+      id: 2,
+      kind: 65536,
+      name: '__type',
+      url: 'globals.html#requestdriver.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported',
+      parent: 'RequestDriver'
+    },
+    {
+      id: 3,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/apirequest.html#rawrequest.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'ApiRequest.rawRequest'
+    },
+    {
+      id: 4,
+      kind: 65536,
+      name: '__type',
+      url: 'globals.html#responsemodifier.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported',
+      parent: 'ResponseModifier'
+    },
+    {
+      id: 5,
+      kind: 65536,
+      name: '__type',
+      url: 'globals.html#requesterrormodifier.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported',
+      parent: 'RequestErrorModifier'
+    },
+    {
+      id: 6,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/user.html#mobilephonenumber.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'User.mobilePhoneNumber'
+    },
+    {
+      id: 7,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/waitinglist.html#billingvariables.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'WaitingList.billingVariables'
+    },
+    {
+      id: 8,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/updatewaitinglist.html#billingvariables.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'UpdateWaitingList.billingVariables'
+    },
+    {
+      id: 9,
+      kind: 65536,
+      name: '__type',
+      url: 'classes/deferredpromise.html#resolve.__type-1',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'DeferredPromise.resolve'
+    },
+    {
+      id: 10,
+      kind: 65536,
+      name: '__type',
+      url: 'classes/deferredpromise.html#reject.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'DeferredPromise.reject'
+    },
+    {
+      id: 11,
+      kind: 65536,
+      name: '__type',
+      url: 'globals.html#promisefn.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported',
+      parent: 'PromiseFn'
+    },
+    {
+      id: 12,
+      kind: 65536,
+      name: '__type',
+      url: 'globals.html#conditionfn.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported',
+      parent: 'ConditionFn'
+    },
+    {
+      id: 13,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/userdata.html#name.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'UserData.name'
+    },
+    {
+      id: 14,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/authenticationsuccess.html#token.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'AuthenticationSuccess.token'
+    },
+    {
+      id: 15,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/session.userdata.html#name.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'session.UserData.name'
+    },
+    {
+      id: 16,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/admin.waitinglist.html#billingvariables.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'admin.WaitingList.billingVariables'
+    },
+    {
+      id: 17,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/paymentsystem.html#configuration.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'PaymentSystem.configuration'
+    },
+    {
+      id: 18,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/payment.paymentsystem.html#configuration.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'payment.PaymentSystem.configuration'
+    },
+    {
+      id: 19,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/seatersexceptionv3.validationerror.html#error.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'SeatersExceptionV3.ValidationError.error'
+    },
+    {
+      id: 20,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/seatersexceptionv3.apiexception.html#errors.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-property tsd-is-not-exported',
+      parent: 'SeatersExceptionV3.ApiException.errors'
+    },
+    {
+      id: 21,
+      kind: 65536,
+      name: '__type',
+      url: 'interfaces/seatersexceptionv3.apiexception.html#errors.__type.errorstypes.__type-1',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-variable tsd-is-not-exported',
+      parent: 'SeatersExceptionV3.ApiException.errors.__type.errorsTypes'
+    },
+    {
+      id: 22,
+      kind: 65536,
+      name: '__type',
+      url: 'globals.html#promisemiddleware.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported',
+      parent: 'PromiseMiddleware'
+    },
+    {
+      id: 23,
+      kind: 65536,
+      name: '__type',
+      url: 'globals.html#getseatersclient.__type',
+      classes: 'tsd-kind-type-literal tsd-parent-kind-variable tsd-is-not-exported',
+      parent: 'getSeatersClient'
+    },
+    { id: 24, kind: 32, name: 'version', url: 'globals.html#version', classes: 'tsd-kind-variable' }
+  ]
+};
