201a0d11d265c40916706942138414f341622bc2
sanderdecoster <sander.decoster@seaters.com>
2018-03-23 11:59:48 +0100

    Exported types

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ export const version: string = '${package.version}';
 
 export * from './seaters-client';
 export * from './shared-types';
-export { pub } from './services/public-service/public-types';
-export { fan } from './services/fan-service/fan-types';
-export { profiling } from './services/fan-service/profiling-types';
-export { survey } from './services/fan-service/survey-types';
-export { session } from './services/session-service/session-types';
+
 export { admin } from './services/admin-service';
+export { app } from './services/app-service';
+export { fan } from './services/fan-service/fan-types';
+export { payment } from './services/payment-service';
+export { pub } from './services/public-service/public-types';
+export { profiling } from './services/fan-service/profiling-types';
+export { session } from './services/session-service/session-types';
+export { survey } from './services/fan-service/survey-types';
+export { ticketing } from './services/ticketing-service';
