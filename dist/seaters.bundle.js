25d8229e8cffc9ac6b99c011d24b585b026d56a7
caroline.vanaerschot <caroline.vanaerschot@seaters.com>
2022-03-03 15:15:00 +0100

    Commit version 1.36.2

--- dist/seaters.bundle.js
+++ dist/seaters.bundle.js
@@ -2644,7 +2644,7 @@ var SeatersSDK = /******/ (function(modules) {
       Object.defineProperty(exports, '__esModule', { value: true });
       //noinspection TsLint
       // tslint:disable-next-line
-      exports.version = '1.36.02';
+      exports.version = '1.36.2';
       __export(__webpack_require__(22));
       var fan_types_1 = __webpack_require__(2);
       exports.fan = fan_types_1.fan;
